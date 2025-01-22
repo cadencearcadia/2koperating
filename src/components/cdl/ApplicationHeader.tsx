1264969ebcb25679c7c18b6442fa102204999ae4
cadencearcadia <160976331+cadencearcadia@users.noreply.github.com>
2025-01-22 10:56:25 -0800

    fixed image location

--- src/components/cdl/ApplicationHeader.tsx
+++ src/components/cdl/ApplicationHeader.tsx
@@ -13,7 +13,7 @@ const ApplicationHeader = () => {
       </div>
       <div className="hidden md:block">
        <img 
-  src="/public/trucker-tanker.webp" 
+  src="/trucker-tanker.webp" 
   alt="Oil & Gas Operations" 
   className="w-full h-64 object-contain rounded-lg shadow-lg"
 />
